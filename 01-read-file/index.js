beab5b241b8a04c31a883800f2be2a3e85a42bc1
Andrey Kulish <andreykulish89@gmail.com>
2022-05-23 22:37:52 +0300

    01-read-file DONE

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,8 @@
-console.log("Hello node!!!");
+const fs = require("fs");
+const path = require("path");
+const stream = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
+
+let data = "";
+
+stream.on("data", (chunk) => (data += chunk));
+stream.on("end", () => console.log(data));
