848fe8ceee45e468fd914fef64972a1d388891b1
Andrey Kulish <andreykulish89@gmail.com>
2022-05-24 21:57:55 +0300

    4 task complete

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,30 +1,31 @@
 const path = require("path");
 const fs = require("fs");
+const myPath = path.join(__dirname, "secret-folder");
 
 function getFilesinFolder(pathToFolder) {
   fs.readdir(pathToFolder, { withFileTypes: true }, (error, files) => {
     if (!error) {
       for (const file of files) {
         if (file.isDirectory()) {
-          getFilesinFolder(path.join(__dirname, "secret-folder", "image.jpg"));
+          pathToFolder = path.join(pathToFolder, file.name);
+          getFilesinFolder(pathToFolder);
         } else {
-          // fs.stat("index.js",
-          //   (err, stats) => {
-          //     if (err) console.log(err);
-          //     else {
-          //       console.log(stats.size);
-          //     }
-          //   }
-          // );
-          if (path.parse(file.name).ext) {
-            console.log(
-              `${path.parse(file.name).name} - ${path
-                .parse(file.name)
-                .ext.slice(1)}`
-            );
-          } else {
-            console.log(`${path.parse(file.name).name.slice(1)}`);
-          }
+          fs.stat(path.join(pathToFolder, file.name), (err, stats) => {
+            if (err) console.log(err);
+            else {
+              if (path.parse(file.name).ext) {
+                console.log(
+                  `${path.parse(file.name).name} - ${path
+                    .parse(file.name)
+                    .ext.slice(1)} - ${stats.size}`
+                );
+              } else {
+                console.log(
+                  `${path.parse(file.name).name.slice(1)} - ${stats.size}`
+                );
+              }
+            }
+          });
         }
       }
     } else {
@@ -32,4 +33,4 @@ function getFilesinFolder(pathToFolder) {
     }
   });
 }
-getFilesinFolder(path.join(__dirname, "secret-folder"));
+getFilesinFolder(myPath);
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const fs = require("fs");
+const path = require("path");
 
+const copyDir = (dirSource, dirTarget) => {
+  fs.rm(dirTarget, { recursive: true }, (err) => {
+    fs.promises
+      .mkdir(dirTarget, { recursive: true })
+      .then(function () {
+        console.log("Copy dir " + dirSource + " to " + dirTarget);
+
+        fs.promises.readdir(dirSource, { withFileTypes: true }).then((files) =>
+          files.forEach((v) => {
+            if (v.isFile()) {
+              fs.promises.copyFile(
+                path.join(dirSource, v.name),
+                path.join(dirTarget, v.name)
+              );
+            } else if (v.isDirectory()) {
+              copyDir(
+                path.join(dirSource, v.name, "/"),
+                path.join(dirTarget, v.name, "/")
+              );
+            }
+          })
+        );
+      })
+      .catch(function () {
+        console.log("Failed to create directory!");
+      });
+  });
+};
+
+console.log("Application start!");
+
+const source = path.join(__dirname, "files/");
+const target = path.join(__dirname, "files-copy/");
+
+copyDir(source, target);
