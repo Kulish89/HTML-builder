dc69abd992adcba728f55c3415458f16f1530b6a
Andrey Kulish <andreykulish89@gmail.com>
2022-05-25 11:13:07 +0300

    fix: 02-write-file add keyword exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,24 +1,21 @@
 const fs = require("fs");
-const { format } = require("path");
+const process = require("process");
 const path = require("path");
-const { stdout, stdin } = process;
 
 const writeStream = fs.createWriteStream(path.join(__dirname, "text.txt"));
 
-stdout.write("Hello my friend! You can write your message.\n");
-const readStream = fs.createReadStream(
-  path.join(__dirname, "text.txt"),
-  "utf-8"
-);
+process.stdout.write("Hello my friend! You can write your message.\n");
 
-stdin.on("data", (data) => {
-  const buffer = Buffer.from("exit");
-  if (data == buffer) {
+process.stdin.on("data", (data) => {
+  if (data.toString().trim() == "exit") {
     process.exit(0);
   } else if (data.toString().length === 2) {
-    stdout.write("Please, inter your message!!!\n");
+    process.stdout.write("Please, inter your message!!!\n");
   } else {
     writeStream.write(data);
   }
 });
-process.on("exit", () => console.log("Ok, good by!"));
+process.on("exit", () => console.log("Ok, good bye!"));
+process.on("SIGINT", () => {
+  process.exit(0);
+});
